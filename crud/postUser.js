e63a5c38c9deafdc0e2e9fcc3c3ac1ca672adb3a
Teddyburgonde <bandama.teddy@gmail.com>
2025-03-20 11:12:03 +0100

    added method post , to do verif

--- crud/postUser.js
+++ crud/postUser.js
@@ -1,1 +1,49 @@
 import db from './db.js';
+
+
+const addUserHandler = (req, reply) =>
+{
+	const {username, email, password} = req.body;
+	if (!username || !email || !password)
+		return reply.status(400).send({message: "All fields are mandatory."});
+	db.get("SELECT * FROM users WHERE email = ?", [email], (err, row) =>
+	{
+		if (row)
+			return reply.status(400).send({message: "This email already exists."});
+		db.run('INSERT INTO users(username, email, password) VALUES (?, ?, ?)', [username, email, password], (err) => 
+		{
+			if (err)
+				return reply.status(500).send({ message: "Database error" });
+			reply.send('User added');	
+		});
+	});
+};
+	
+const addUserSchema = 
+{
+	body:
+	{
+		type: 'object',
+		required: ['username', 'email', 'password'],
+		properties:
+		{
+		  username: { type: 'string' },
+		  email: { type: 'string' },
+		  password: { type: 'string' },
+		},
+	},
+	response: 
+	{
+		200: { type: 'string' },
+	},	
+};
+	
+	
+const addUserOpts=
+{
+	schema: addUserSchema,
+	handler: addUserHandler,
+};
+	
+// create routes and use a constant that we've put a schema and a function in 
+fastify.post('/users', addUserOpts);
